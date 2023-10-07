9c95d8acec68a4f0b5a4b419dd43f03eb2f4ebd7
Vishwjeet Singh <128573304+Vishwjeet123@users.noreply.github.com>
2023-10-07 05:12:52 +0000

    today

--- variables.js
+++ variables.js
@@ -1,1 +1,15 @@
-console.log("Hey Vishwjeet")
+console.log("Hey Vishwjeet");
+
+//define variable
+let userEmailID = "svishwjeet737@gmail.com"
+var userpass = "1234567890"
+const userID = "098765532"
+userCity = "new delhi"
+let userstate;
+
+
+userEmailID ="svishwjeet737@gmail.com"//iski values change ho jaygi
+userpass = "1234567890"//iski v valu change ho jaygi
+userID = "098765532"//iski valuse change nhi hogi
+userCity = "new delhi"
+console.log(userEmailID)
